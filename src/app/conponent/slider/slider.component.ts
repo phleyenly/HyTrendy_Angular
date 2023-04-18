557ee0b079b4216162e02e675176569b495be16d
phleyenly <129738023+phleyenly@users.noreply.github.com>
2023-04-18 13:33:00 +0700

    amend slider-product-footer

--- src/app/conponent/slider/slider.component.ts
+++ src/app/conponent/slider/slider.component.ts
@@ -1,31 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit{
+  ngOnInit(): void {
+    setInterval(() =>{
+      const currentIndex = this.imageSliderArry.indexOf(this.imageSlider);
+      const nextIndex = (currentIndex +1 ) >= this.imageSliderArry.length ? 0 : (currentIndex +1);
+      this.changeSlider(nextIndex);
+    }, 4000)
+  }
+  imageSliderArry: string[] =[
+    'assets/client/img/slider/slide1.jpg',
+    'assets/client/img/slider/slide2.jpg',
+    'assets/client/img/slider/slide3.jpg'
+  ]
 
-  imageSlider : string = 'assets/client/img/slider/slide1.jpg';
+  imageSlider : string = this.imageSliderArry[0];
   
   textSliderArry: string[] = [
     "<h3>MEGA SALE -30%</h3>" +
     "<p>Winter collection for women's. <br>We all have choices for you. Check it out!</p>" +
-    '<p class="carousel-more-wrap">' +
-    '<a class="carousel-more btn btn-dark" href="#">View collection</a> ' +
+    '<p class="fr-slider-more-wrap">' +
+    '<a class="fr-slider-more" href="#">View collection</a> ' +
      '</p>' ,
      
      "<h3>NEW COLLECTION</h3>" +
       "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>Aliquam consequuntur dolorem doloribus fuga harum</p>" +
-      '<p class="carousel-more-wrap">' +
-       '<a class="carousel-more btn btn-dark" href="#">Shopping now</a>' +
+      '<p class="fr-slider-more-wrap">' +
+       '<a class="fr-slider-more" href="#">Shopping now</a>' +
         '</p>'   ,
         
        "<h3>SUMMER TRENDS</h3>" +
       "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br>Aliquam consequuntur dolorem doloribus fuga harum</p>"+
-      '<p class="carousel-more-wrap">' +
-      '<a class="carousel-more btn btn-dark" href="#">Start shopping</a>'  +
+      '<p class="fr-slider-more-wrap">' +
+      '<a class="fr-slider-more" href="#">Start shopping</a>'  +
       '</p>'
 
   ]
@@ -37,7 +49,7 @@ export class SliderComponent {
   changeSlider(index: number) {
     switch(index){
       case 0:
-        this.imageSlider= 'assets/client/img/slider/slide1.jpg';
+        this.imageSlider= this.imageSliderArry[0];
         this.textSlider = this.textSliderArry[0];
         this.showActive1 = 'active';
         this.showActive2 = '';
@@ -46,7 +58,7 @@ export class SliderComponent {
       break;
 
       case 1:
-        this.imageSlider= 'assets/client/img/slider/slide2.jpg';
+        this.imageSlider= this.imageSliderArry[1];
         this.textSlider = this.textSliderArry[1];
         this.showActive1 = '';
         this.showActive2 = 'active';
@@ -54,7 +66,7 @@ export class SliderComponent {
       break;
 
       case 2:
-        this.imageSlider= 'assets/client/img/slider/slide3.jpg';
+        this.imageSlider= this.imageSliderArry[2];
         this.textSlider = this.textSliderArry[2];
         this.showActive1 = '';
         this.showActive2 = '';
