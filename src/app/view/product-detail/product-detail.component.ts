f5fb54b2aa75b4baddea2d91d31db968220914bf
phleyenly <129738023+phleyenly@users.noreply.github.com>
2023-04-24 17:22:59 +0700

    product-detail-getCatefgoryandType

--- src/app/view/product-detail/product-detail.component.ts
+++ src/app/view/product-detail/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Category } from 'src/app/interface/category';
 import { Product } from 'src/app/interface/product';
+import { CategoryService } from 'src/app/service/category.service';
 import { ProductService } from 'src/app/service/product.service';
 import { TypeService } from 'src/app/service/type.service';
 
@@ -11,27 +13,38 @@ import { TypeService } from 'src/app/service/type.service';
 })
 export class ProductDetailComponent implements OnInit {
   product : Product = {id:-1 , name : " ", price: 0, stock:0, size: [], tags: "", origin: "",description: "",image: [] , material: ""};
-  tiltle: string[] = [];
+  title: string[] = [];
+  category: Category = {id: 0, name: '', code:'', types:[]};
   types: string = "";
-  categoryCode = this.route.snapshot.queryParamMap.get('categoryCode') 
-  typeCode = this.route.snapshot.queryParamMap.get('typeCode')
+  categoryCode = this.route.snapshot.queryParamMap.get('categoryCode') || '';
+  typeCode = this.route.snapshot.queryParamMap.get('typeCode') || '';
 
   constructor(
     private productService: ProductService,
     private typeService: TypeService,
+    private categoryService: CategoryService,
     private route: ActivatedRoute,
 ){}
-  ngOnInit(): void {
+  async ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id') ;
     const idd = id || '';
     const numberId = parseInt(idd);
     this.getById(numberId);
-    const type = this.typeCode || '';
+    this.title.push("Tất cả sản phẩm");
+    if(this.categoryCode !== '' && this.typeCode !== '') {
+      console.log(this.categoryCode)
+      await this.getCategoryByCode(this.categoryCode);
+      await this.getTypeByCode(this.categoryCode, this.typeCode);
+      this.title.push(this.category.name);
+      this.title.push(this.types);
+    }
+      
     
 
     setTimeout(() => {
       console.log(this.product);
       console.log(this.typeCode);
+      console.log(this.title);
       
       
     }, 5000);
@@ -44,10 +57,12 @@ export class ProductDetailComponent implements OnInit {
     })
   }
 
-  // getTypeByCode(typeCode: string) {
-  //   this.typeService.getTypeByCode(typeCode).subscribe((t: any) => {
-  //     this.types = t;
-  //   })
-  // }
+  async getCategoryByCode(categoryCode: string) {
+    this.category = await this.categoryService.getCategoryByCode(categoryCode).toPromise();
+  }
+
+  async getTypeByCode(categoryCode: string, typeCode: string) {
+    this.types = await this.typeService.getTypeByCode(categoryCode, typeCode).toPromise();
+  }
 
 }
