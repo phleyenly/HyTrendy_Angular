f80939086fb1652ae3525f46437ed9bdeb3a53fe
phleyenly <129738023+phleyenly@users.noreply.github.com>
2023-05-18 16:42:38 +0700

    cart

---
+++ src/app/service/cart.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cart } from '../interface/cart';
+
+const baseUrl = "http://localhost:8080/api/cart";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+
+  constructor(private http: HttpClient) { }
+
+  getCart() : Observable <Cart[]> {
+    return this.http.get<Cart[]>(baseUrl);
+
+  }
+}
--- src/app/view/cart/cart.component.ts
+++ src/app/view/cart/cart.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/interface/cart';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cart: Cart[] = [];
+
+  constructor( private cartService: CartService){}
+
+  ngOnInit(): void {
+    this.getCart();
+
+    setTimeout(() => {
+      console.log(this.cart[0].quantity)
+    }, 5000);
+  }
+
+  getCart() {
+    this.cartService.getCart().subscribe((c: Cart[]) =>{
+      this.cart = c;
+    })
+  }
+
+  up(index: number) {
+    
+    if(this.cart[index].quantity <10) {
+       this.cart[index].quantity++
+    }
+   
+  }
+
+  down(index: number) {
+    if(this.cart[index].quantity > 1) {
+       this.cart[index].quantity--;
+    }
+   
+  }
 
 }
