3e6f60820ecebb3a10dd863b74e8a18bb66b2eea
phleyenly <129738023+phleyenly@users.noreply.github.com>
2023-05-19 22:51:24 +0700

    add-cart-to-product-detail

--- src/app/view/product-detail/product-detail.component.ts
+++ src/app/view/product-detail/product-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Category } from 'src/app/interface/category';
+import { InforCreateCart } from 'src/app/interface/infor-create-cart';
 import { Product } from 'src/app/interface/product';
+import { CartService } from 'src/app/service/cart.service';
 import { CategoryService } from 'src/app/service/category.service';
 import { ProductService } from 'src/app/service/product.service';
 import { TypeService } from 'src/app/service/type.service';
@@ -18,12 +20,15 @@ export class ProductDetailComponent implements OnInit {
   types: string = "";
   categoryCode = this.route.snapshot.queryParamMap.get('categoryCode') || '';
   typeCode = this.route.snapshot.queryParamMap.get('typeCode') || '';
+  quantity: number = 1;
+  cart: InforCreateCart = {id: -1, quantity: 1, size: 'M'};
 
   constructor(
     private productService: ProductService,
     private typeService: TypeService,
     private categoryService: CategoryService,
     private route: ActivatedRoute,
+    private cartService: CartService
 ){}
   async ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id') ;
@@ -63,6 +68,31 @@ export class ProductDetailComponent implements OnInit {
 
   async getTypeByCode(categoryCode: string, typeCode: string) {
     this.types = await this.typeService.getTypeByCode(categoryCode, typeCode).toPromise();
+  }
+
+  up() {
+    if(this.quantity<10) {
+      this.quantity++
+    }
+    
+  }
+
+  down() {
+    if(this.quantity>1) {
+      this.quantity--
+    }
+  }
+
+  selectSize(size: string)  {
+    this.cart.size = size;
+  }
+
+  createCart() {
+    this.cart.id = this.product.id;
+    this.cart.quantity = this.quantity;
+    this.cartService.createCart(this.cart).subscribe((m: any) => {
+      alert(m.message)
+    })
   }
 
 }
