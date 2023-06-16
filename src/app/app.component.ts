caeea8465ca8c0e11b3a886bec1250c8b423de68
phleyenly <129738023+phleyenly@users.noreply.github.com>
2023-06-16 23:41:39 +0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NotifierService } from 'angular-notifier';
+import { AuthenticationService } from './service/authentication.service';
 // import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -10,14 +11,31 @@ import { NotifierService } from 'angular-notifier';
 export class AppComponent implements OnInit {
   title = 'HyTrendy_Angular';
   isAdminPage = false;
+  isRoleAdmin = false;
   // constructor(private route: ActivatedRoute) {}
-  constructor(private notifier: NotifierService) {}
+  constructor(private notifier: NotifierService, private authenService: AuthenticationService) {}
 
   ngOnInit() {
     if(window.location.pathname.startsWith('/admin')) {
       this.isAdminPage = true
+      const token = sessionStorage.getItem('token')
+      if(token) {
+        this.authenService.authentication().subscribe (
+          data => {
+            if(data.role !== 'ADMIN') {
+              window.location.href = '/login'
+            } else {
+              this.isRoleAdmin = true
+            }
+          },
+          err => {
+            window.location.href = '/login'
+          }
+        )
+      } else {
+        window.location.href = '/login'
+      }
     }
-    console.log(this.isAdminPage)
 
     // setTimeout(() => {
     //   this.notifier.notify('success', 'Em ghét Hiệp thúi','notifier-icon-success')
